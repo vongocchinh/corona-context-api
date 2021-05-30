7441433cf1f9dd39503e31099ed76ec37a08adc9
{vongocchinh} <{ngocchinh1410@gmail.com}>
2021-05-30 13:29:38 +0700

    upada

--- src/myApp/Api/index.js
+++ src/myApp/Api/index.js
@@ -3,25 +3,25 @@ export const fetchGet = async () => {
   try {
     const { data } = await axios("https://corona.lmao.ninja/v2/all");
     // console.log(data);
-    var dataItem = {
-      date: data.updated,
-      cases: data.cases,
-      recovered: data.recovered,
-      deaths: data.deaths,
-      affectedCountries: data.affectedCountries,
-      population: data.population,
-      casesPerOneMillion: data.casesPerOneMillion,
-      deathsPerOneMillion: data.deathsPerOneMillion,
-      oneCasePerPeople: data.oneCasePerPeople,
-      oneDeathPerPeople: data.oneDeathPerPeople,
-      oneTestPerPeople: data.oneTestPerPeople,
-      recoveredPerOneMillion: data.recoveredPerOneMillion,
-      tests: data.tests,
-      testsPerOneMillion: data.testsPerOneMillion,
-      todayCases: data.todayCases,
-      todayDeaths: data.todayDeaths,
-      todayRecovered: data.todayRecovered,
-    };
+    // var dataItem = {
+    //   date: data.updated,
+    //   cases: data.cases,
+    //   recovered: data.recovered,
+    //   deaths: data.deaths,
+    //   affectedCountries: data.affectedCountries,
+    //   population: data.population,
+    //   casesPerOneMillion: data.casesPerOneMillion,
+    //   deathsPerOneMillion: data.deathsPerOneMillion,
+    //   oneCasePerPeople: data.oneCasePerPeople,
+    //   oneDeathPerPeople: data.oneDeathPerPeople,
+    //   oneTestPerPeople: data.oneTestPerPeople,
+    //   recoveredPerOneMillion: data.recoveredPerOneMillion,
+    //   tests: data.tests,
+    //   testsPerOneMillion: data.testsPerOneMillion,
+    //   todayCases: data.todayCases,
+    //   todayDeaths: data.todayDeaths,
+    //   todayRecovered: data.todayRecovered,
+    // };
     return data;
   } catch (error) {
     console.log(error);
